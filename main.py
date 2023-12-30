f8d0651a2ee3376396ed184f50de5d8efb4fdc4d
ZeyaKim <chlwltjrdev@gmail.com>
2023-12-30 16:03:23 +0900

    feat: add widgets to layout

--- main.py
+++ main.py
@@ -1,36 +1,43 @@
 import logging
 import os
 import sys
+
 from PySide6.QtWidgets import QApplication
+
 from src.ui.main_window import MainWindow
 
+
 def init_logger():
-    os.makedirs('logs', exist_ok=True)
+    os.makedirs("logs", exist_ok=True)
 
     # 로거 생성 및 레벨 설정
-    logger = logging.getLogger('root')
+    logger = logging.getLogger("root")
     logger.setLevel(logging.DEBUG)
 
     # 콘솔 핸들러 생성 및 설정
     console_handler = logging.StreamHandler()
     console_handler.setLevel(logging.INFO)
-    console_formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(module)s :: %(message)s', 
-                                          '%Y-%m-%d %H:%M:%S')
+    console_formatter = logging.Formatter(
+        "%(asctime)s :: %(levelname)s :: %(module)s :: %(message)s", "%Y-%m-%d %H:%M:%S"
+    )
     console_handler.setFormatter(console_formatter)
 
     # 파일 핸들러 생성 및 설정
-    file_handler = logging.FileHandler('logs/error.log')
+    file_handler = logging.FileHandler("logs/error.log")
     file_handler.setLevel(logging.ERROR)
-    file_formatter = logging.Formatter('%(asctime)s :: %(levelname)s :: %(module)s :: %(lineno)s\n%(message)s',
-                                       '%Y-%m-%d %H:%M:%S')
+    file_formatter = logging.Formatter(
+        "%(asctime)s :: %(levelname)s :: %(module)s :: %(lineno)s\n%(message)s",
+        "%Y-%m-%d %H:%M:%S",
+    )
     file_handler.setFormatter(file_formatter)
 
     # 로거에 핸들러 추가
     logger.addHandler(console_handler)
     logger.addHandler(file_handler)
-    logger.info('Logger initialized')
+    logger.info("Logger initialized")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     init_logger()
     app = QApplication(sys.argv)
 
--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1,15 +1,39 @@
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import (QListWidget, QMainWindow, QPushButton,
+                               QVBoxLayout, QWidget, QPlainTextEdit)
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.init_ui()
-        
+        self.paths = []
+
     def init_ui(self):
-        self.setWindowTitle('Work Hard Hassan!')
+        self.setWindowTitle("Work Hard Hassan!")
         self.setGeometry(100, 100, 800, 600)
+
+        layout = QVBoxLayout()
+
+        self.add_path_button = QPushButton("경로 추가", self)
+        self.add_path_button.clicked.connect(self.add_path)
+        layout.addWidget(self.add_path_button)
+
+        self.added_paths_list = QListWidget(self)
+        layout.addWidget(self.added_paths_list)
         
-        ...
+        self.execute_button = QPushButton("실행", self)
+        layout.addWidget(self.execute_button)
 
-
-
+        self.progress_log_viewer = QPlainTextEdit(self)
+        self.progress_log_viewer.setReadOnly(True)
+        layout.addWidget(self.progress_log_viewer)
+        
+        container = QWidget()
+        container.setLayout(layout)
+        self.setCentralWidget(container)
+        
+        for i in range(20):
+            self.progress_log_viewer.appendPlainText(f"{i}번째 줄")
+        
+    def add_path(self):
+        pass
