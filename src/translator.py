603c674cb0858667a1e0120c2541b7489f8996a0
ZeyaKim <chlwltjrdev@gmail.com>
2023-12-31 13:02:00 +0900

    feat: translate
    대본을 받아 라인마다 번역한 후 별도의 파일에 저장하는 기능을 구현

--- src/translator.py
+++ src/translator.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 import deepl
 import toml
@@ -6,7 +7,8 @@ import toml
 
 class Translator:
     def __init__(self):
-        ...
+        if self.is_valid_api_key(self.load_api_key()):
+            self.translator = deepl.Translator(self.load_api_key())
 
     def load_api_key(self):
         with open("keys.toml", "r") as f:
@@ -30,8 +32,43 @@ class Translator:
         if self.is_valid_api_key(new_api_key):
             config = toml.load("keys.toml")
             config["api_key"]["deepl"] = new_api_key
+            self.translator = deepl.Translator(new_api_key)
             with open("keys.toml", "w") as f:
                 toml.dump(config, f)
                 logging.info(f"API key is edited, new API key is {new_api_key}")
         else:
             logging.info("Failed to edit API key")
+
+    def translate(self, transcription, file_path):
+        file_name = os.path.basename(file_path).split(".")[0]
+        parent_folder_path = os.path.dirname(file_path)
+
+        translated_transcription = []
+        try:
+            with open(
+                os.path.join(parent_folder_path, f"{file_name}_translated.txt"), "w"
+            ) as f:
+                for sentence in transcription:
+                    try:
+                        translated_text = str(
+                            self.translate_text(sentence["text"], target_lang="KO")
+                        )
+                        
+                        translated_transcription.append({
+                            'start': sentence['start'],
+                            'end': sentence['end'],
+                            'translated_text': translated_text
+                        })
+                        f.write(
+                            f"{sentence['start']} ~ {sentence['end']}\n{translated_text}\n\n"
+                        )
+                    except Exception as exc:
+                        error_msg = f"Failed to translate text {sentence['text']}: {exc}"
+                        logging.error(error_msg)
+        except Exception as exc:
+            error_msg = f"Failed to making translated transcription file from {file_path}: {exc}"
+            logging.error(error_msg)
+        
+        return translated_transcription
+        
+        
