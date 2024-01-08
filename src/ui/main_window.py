79250c6e1bb1f6f2a8e832189de3ce6b1228b0eb
ZeyaKim <chlwltjrdev@gmail.com>
2024-01-08 22:54:25 +0900

    feat: dependency injection

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -1,11 +1,12 @@
+import logging
+
+from dependency_injector import containers, providers
 from PySide6.QtGui import QAction, QIcon
 from PySide6.QtWidgets import (
     QApplication,
-    QFileDialog,
-    QHBoxLayout,
     QMainWindow,
     QMenu,
-    QPushButton,
+    QPlainTextEdit,
     QSystemTrayIcon,
     QVBoxLayout,
     QWidget,
@@ -15,69 +16,55 @@ from src.audio_extractor import AudioExtractor
 from src.subtitle_generator import SubtitleGenerator
 from src.translator import Translator
 
-from .execute_layout import ExecuteLayout
-from .path_table import PathTable
+from .path_panel import PathPanel
+from .setting_panel import SettingPanel
+
+
+class Container(containers.DeclarativeContainer):
+    translator = providers.Singleton(Translator)
+    audio_extractor = providers.Singleton(AudioExtractor)
+    subtitle_generator = providers.Singleton(SubtitleGenerator)
+    log_viewer = providers.Singleton(QPlainTextEdit)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.translator = Translator()
-        self.audio_extractor = AudioExtractor()
-        self.subtitle_generator = SubtitleGenerator()
-        self.setWindowIcon(QIcon("assets/icon/hassan_icon.png"))
-
         self.init_ui()
 
-    def init_ui(self):
+    def init_ui(self) -> None:
+        self.setWindowIcon(QIcon("assets/icon/hassan_icon.png"))
         self.setWindowTitle("Work Hard Hassan!")
         self.setGeometry(100, 100, 1080, 720)
 
-        layout = QVBoxLayout()
+        container = Container()
 
-        path_button_layout = QHBoxLayout()
-
-        self.add_file_paths_button = QPushButton("파일 추가", self)
-        self.add_file_paths_button.clicked.connect(self.add_file_paths)
-        path_button_layout.addWidget(self.add_file_paths_button)
-
-        self.add_folder_paths_button = QPushButton("폴더 추가", self)
-        self.add_folder_paths_button.clicked.connect(self.add_folder_paths)
-        path_button_layout.addWidget(self.add_folder_paths_button)
-
-        layout.addLayout(path_button_layout)
-
-        self.path_table = PathTable()
-        layout.addWidget(self.path_table)
-
-        execute_setting_layout = ExecuteLayout(
-            self.audio_extractor, self.translator, self.subtitle_generator
+        path_panel = PathPanel(
+            container.log_viewer(),
         )
 
-        layout.addLayout(execute_setting_layout)
+        setting_panel = SettingPanel(
+            container.audio_extractor(),
+            container.translator(),
+            container.subtitle_generator(),
+            container.log_viewer(),
+        )
 
-        container = QWidget()
-        container.setLayout(layout)
-        self.setCentralWidget(container)
+        log_viewer = container.log_viewer()
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(path_panel)
+        layout.addWidget(setting_panel)
+        layout.addWidget(log_viewer)
+
+        widget_container = QWidget()
+        widget_container.setLayout(layout)
+        self.setCentralWidget(widget_container)
 
         self.create_tray_icon()
 
-    def add_file_paths(self):
-        file_dialog = QFileDialog(self)
-        file_paths = file_dialog.getOpenFileNames(
-            self, "Select Audio Files", "", "Audio Files (*.mp3 *.wav)"
-        )
-
-        for selected_file_path in file_paths[0]:
-            self.path_table.add_new_path(selected_file_path, path_type="file")
-
-    def add_folder_paths(self):
-        folder_dialog = QFileDialog(self)
-        selected_folder_path = folder_dialog.getExistingDirectory(self, "Select Folder")
-
-        self.path_table.add_new_path(selected_folder_path, path_type="folder")
-
-    def create_tray_icon(self):
+    def create_tray_icon(self) -> None:
         self.tray_icon = QSystemTrayIcon(self)
         self.tray_icon.setIcon(QIcon("assets/icon/hassan_icon.png"))
 
@@ -93,11 +80,18 @@ class MainWindow(QMainWindow):
         self.tray_icon.setContextMenu(tray_menu)
         self.tray_icon.show()
 
-    def closeEvent(self, event):
+    def closeEvent(self, event) -> None:
         if QApplication.activeWindow() is not None:
-            # 애플리케이션이 활성 상태일 때만 메시지를 출력
             self.tray_icon.showMessage(
                 "Work Hard Hassan!", "하산이 야근하고 있습니다!", QSystemTrayIcon.Information, 2000
             )
         event.ignore()
         self.hide()
+
+    def add_log(self, message, log_level) -> None:
+        match log_level:
+            case "info":
+                logging.info(message)
+            case "error":
+                logging.error(message)
+        self.log_viewer.appendPlainText(message)
