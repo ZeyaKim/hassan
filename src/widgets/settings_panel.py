f3f4d592a0051f73f9c59dd4f89a9a86ad2f26ac
ZeyaKim <chlwltjrdev@gmail.com>
2024-02-01 16:28:51 +0900

    feat: init_ui

--- src/widgets/settings_panel.py
+++ src/widgets/settings_panel.py
@@ -18,21 +18,22 @@ class SettingsPanel(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
 
-        group_box = QGroupBox("Settings")
-        group_box_layout = QVBoxLayout()
-        group_box.setLayout(group_box_layout)
+        settings_group_box = self.init_settings_ui()
 
-        translator_group_box = QGroupBox("Translator")
-        translator_layout = QVBoxLayout()
-        translator_group_box.setLayout(translator_layout)
+        layout.addWidget(settings_group_box)
 
-        group_box_layout.addWidget(translator_group_box)
+    def init_settings_ui(self):
+        settings_group_box = QGroupBox("Settings")
+        settings_layout = QVBoxLayout()
+        settings_group_box.setLayout(settings_layout)
+
+        translator_group_box = self.init_translator_settings_ui()
+
+        settings_layout.addWidget(translator_group_box)
 
         h_layout = QHBoxLayout()
 
-        audio_extractor_group_box = QGroupBox("Audio Extractor")
-        audio_extractor_layout = QVBoxLayout()
-        audio_extractor_group_box.setLayout(audio_extractor_layout)
+        audio_extractor_group_box = self.init_audio_extractor_settings_ui()
 
         h_layout.addWidget(audio_extractor_group_box)
 
@@ -42,6 +43,27 @@ class SettingsPanel(QWidget):
 
         h_layout.addWidget(subtitle_generator_group_box)
 
-        group_box_layout.addLayout(h_layout)
+        settings_layout.addLayout(h_layout)
 
-        layout.addWidget(group_box)
+        return settings_group_box
+
+    def init_translator_settings_ui(self):
+        translator_group_box = QGroupBox("Translator")
+        translator_layout = QVBoxLayout()
+        translator_group_box.setLayout(translator_layout)
+
+        return translator_group_box
+
+    def init_audio_extractor_settings_ui(self):
+        audio_extractor_group_box = QGroupBox("Audio Extractor")
+        audio_extractor_layout = QVBoxLayout()
+        audio_extractor_group_box.setLayout(audio_extractor_layout)
+
+        return audio_extractor_group_box
+
+    def init_subtitle_generator_settings_ui(self):
+        subtitle_generator_group_box = QGroupBox("Subtitle Generator")
+        subtitle_generator_layout = QVBoxLayout()
+        subtitle_generator_group_box.setLayout(subtitle_generator_layout)
+
+        return subtitle_generator_group_box
