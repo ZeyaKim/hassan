7d142f794188e618ff3e31ac4ccb820711247b20
ZeyaKim <chlwltjrdev@gmail.com>
2024-02-02 03:28:56 +0900

    ♻️extract run_app func

--- src/app.py
+++ src/app.py
@@ -1,30 +1,34 @@
 import logging
 import sys
 
-from dependency_injector.wiring import Provide, inject
-from PySide6.QtWidgets import QApplication, QMainWindow
+from dependency_injector import wiring
+from PySide6 import QtWidgets
 
-from utils.container import Container
-from utils import utils
+from utils import container, utils
 
 
-@inject
-def main(
-    app: QApplication = Provide[Container.app],
-    logger: logging.Logger = Provide[Container.logger],
-    main_window: QMainWindow = Provide[Container.main_window],
-) -> None:
+def main():
+    app_container = container.Container()
 
+    root_dir = app_container.root_dir()
+    app_container.config.from_dict(utils.load_config(root_dir))
+    app_container.wire(modules=[__name__])
+
+    run_application()
+
+
+@wiring.inject
+def run_application(
+    app: QtWidgets.QApplication = wiring.Provide[container.Container.app],
+    logger: logging.Logger = wiring.Provide[container.Container.logger],
+    main_window: QtWidgets.QMainWindow = wiring.Provide[
+        container.Container.main_window
+    ],
+):
     logger.info("Starting application")
-
     main_window.show()
-
     sys.exit(app.exec())
 
 
 if __name__ == "__main__":
-    container = Container()
-    root_dir = container.root_dir()
-    container.config.from_dict(utils.load_config(root_dir))
-    container.wire(modules=[__name__])
     main()
