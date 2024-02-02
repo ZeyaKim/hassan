25fa490f55bc0b56759f78a9126e2fc966be5c93
ZeyaKim <chlwltjrdev@gmail.com>
2024-02-02 14:56:28 +0900

    ✨ : ExtractableExtEnum

--- src/utils/enums.py
+++ src/utils/enums.py
@@ -1,10 +1,14 @@
 from enum import Enum
 
+
 class SubtitleExtEnum(Enum):
     SRT = "srt"
     ASS = "ass"
-    
-    
+
+class ExtractableExtEnum(Enum):
+    MP3 = "mp3"
+    WAV = "wav"
+
 class WhisperModelEnum(Enum):
     SMALL = "small"
     Medium = "medium"
