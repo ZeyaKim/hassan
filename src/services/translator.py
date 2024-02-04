4c0a73591a088be63c57e4f0629af1b45aae2180
ZeyaKim <chlwltjrdev@gmail.com>
2024-02-05 04:59:16 +0900

    ✨ : Translator

---
+++ src/services/translator.py
@@ -0,0 +1,16 @@
+class Translator:
+    def __init__(self, logger, root_dir, config_manager, config):
+        self.logger = logger
+        self.root_dir = root_dir
+        self.config_manager = config_manager
+        self.config = config
+
+    # TODO: api 키를 변경하는 로직을 구현해야 합니다.
+    def change_api_key(self, api_key: str) -> None:
+        if not self.is_valid_api_key(api_key):
+            self.logger.error("Invalid API key")
+            return
+
+    # TODO: api 키를 검증하는 로직을 구현해야 합니다.
+    def is_valid_api_key(self, api_key: str) -> bool:
+        return True
