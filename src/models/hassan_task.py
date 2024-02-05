eaaa5b4a32a7e24c223cb107186f1305fc1613c7
ZeyaKim <chlwltjrdev@gmail.com>
2024-02-06 01:24:07 +0900

    ✨ feat HassanTask.execute

--- src/models/hassan_task.py
+++ src/models/hassan_task.py
@@ -13,12 +13,41 @@ class HassanTask(QStandardItemModel):
         translator,
         subtitle_generator,
         file_path: str,
-        settings: dict,
+        execution_settings: dict,
     ):
         super().__init__()
         self.logger = logger
+        self.root_dir = root_dir
+        self.audio_extractor = audio_extractor
+        self.translator = translator
+        self.subtitle_generator = subtitle_generator
         self.file_path = file_path
+        self.execution_settings = execution_settings
+
         self.parent_dir = os.path.dirname(file_path)
 
     def execute(self):
-        self.logger.debug(f"execute: {self.file_path}")
+        name = os.path.basename(self.file_path)
+
+        transcription = self.audio_extractor.extract_audio(
+            self.file_path,
+            self.parent_dir,
+            name,
+            self.execution_settings["audio_extractor"],
+        )
+        translated_transcription = self.translator.translate_transcription(
+            self.file_path,
+            self.parent_dir,
+            name,
+            self.execution_settings["translator"],
+            transcription,
+        )
+        self.subtitle_generator.generate_subtitle(
+            self.file_path,
+            self.parent_dir,
+            name,
+            self.execution_settings["subtitle_generator"],
+            translated_transcription,
+        )
+
+        self.logger.info(f"Task for {name} has been executed successfully.")
