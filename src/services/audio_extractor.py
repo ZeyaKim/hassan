60343b9d042e142b2e24cf5133072c4dfd89d613
ZeyaKim <chlwltjrdev@gmail.com>
2024-02-26 12:43:56 +0900

    Add AudioExtractor class with transcribe_audio method

---
+++ src/services/audio_extractor.py
@@ -0,0 +1,9 @@
+class AudioExtractor:
+    def __init__(self):
+        self.settings = {"model": "video", "device": "cpu"}
+
+    def transcribe_audio(self, audio_file):
+        pass
+
+    def _save_transcription(self, transcription):
+        pass
