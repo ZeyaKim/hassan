fe2234ce756c1be305f60448891a7c4c27578a96
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-03 22:02:10 +0900

    clear folder

--- main.py
+++
@@ -1,9 +0,0 @@
-from src.cli import cli_commands
-
-
-def main():
-    cli_commands.start_app()
-
-
-if __name__ == "__main__":
-    main()
--- src/utils/enums.py
+++
@@ -1,7 +0,0 @@
-import enum
-
-
-class SupportedAudioFormatEnum(enum.Enum):
-    MP3 = ".mp3"
-    MP4 = ".mp4"
-    WAV = ".wav"
