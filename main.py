be30eca09034b9bfdbc6be743d60d75e0c0bb264
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-03 22:53:30 +0900

    Refactor main.py to use a separate MainWindow class and add start_app function

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication
+from src.gui.main_window import MainWindow
 import sys
+import os
+import pathlib
+
+main_path = __file__
+root_dir = os.environ["ROOT_DIR"] = str(pathlib.Path(main_path).parent.absolute())
+
+
+def start_app():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = QMainWindow()
-    window.show()
-    app.exec()
+    start_app()
