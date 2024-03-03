a0ead1cc7e565130bdab3b66ee708e0d61329e9e
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-03 23:21:26 +0900

    Add logging functionality to main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from src.gui.main_window import MainWindow
 import sys
 import os
 import pathlib
+import logging
+import logging.handlers
 
 main_path = __file__
 root_dir = os.environ["ROOT_DIR"] = str(pathlib.Path(main_path).parent.absolute())
@@ -15,5 +17,34 @@ def start_app():
     sys.exit(app.exec())
 
 
+def init_logging():
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    root_dir = os.environ["ROOT_DIR"]
+    log_path = pathlib.Path(root_dir) / "logs" / "app.log"
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    rotating_file_handler = logging.handlers.RotatingFileHandler(  # 수정된 부분
+        log_path, maxBytes=1024 * 1024 * 10, backupCount=5
+    )
+
+    rotating_file_handler.setLevel(logging.WARNING)  # 수정된 부분
+    rotating_file_handler.setFormatter(formatter)
+
+    logger.addHandler(rotating_file_handler)
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)
+    logger.addHandler(stream_handler)
+
+    logger.info("Logging initialized")
+
+
 if __name__ == "__main__":
+    init_logging()
     start_app()
